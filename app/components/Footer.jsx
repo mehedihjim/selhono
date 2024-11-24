10d5dc9db63b6b3087b2227e482175a3b0c623a0
Mehedi H Jim <mhjim.info@gmail.com>
2024-11-25 05:01:01 +0600

    footer time

---
+++ app/components/Footer.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='pt-[41px] pb-[15px]'>
+            <p className='flex justify-center'>Copyright © <Link href="/">SELHONO</Link></p>
+        </footer>
+    )
+}
+
+export default Footer
